ce05c9bcf7f0d47c8cf7a4f3483913cf6fb6bdf6
David Murphy <davidmurphy@Davids-MacBook-Pro.local>
2020-11-21 17:18:52 +0000

    Working through TDD for Repo

--- src/main/java/Repo.java
+++ src/main/java/Repo.java
@@ -1,2 +1,37 @@
+import java.util.ArrayList;
+
 public class Repo {
+
+    private String name;
+    private String description;
+    private RepoType repoType;
+    private ArrayList<Commit> commits;
+
+    public Repo(String name, String description, RepoType repoType) {
+        this.name = name;
+        this.description = description;
+        this.repoType = repoType;
+        this.commits = new ArrayList<Commit>();
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public RepoType getRepoType() {
+        return this.repoType;
+    }
+
+    public int commitCount() {
+        return this.commits.size();
+    }
+
+    public void addCommit(Commit newCommit) {
+        this.commits.add(newCommit);
+    }
+
 }
--- src/test/java/RepoTest.java
+++ src/test/java/RepoTest.java
@@ -1,2 +1,53 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class RepoTest {
+
+    Repo repo;
+    Commit commit1;
+    Commit commit2;
+    Commit commit3;
+
+    @Before
+    public void setUp() {
+        repo = new Repo("Charles Dozsa", "A Succulent Chinese Meal", RepoType.PUBLIC);
+        commit1 = new Commit("Initial Commit", "1234567890poiuytrewq");
+        commit2 = new Commit("Additional Commit", "0987654321qwertyuiop");
+        commit3 = new Commit("Final Commit", "asdfghjklm0192837465");
+    }
+
+    @Test
+    public void hasName() {
+        assertEquals("Charles Dozsa", repo.getName());
+    }
+
+    @Test
+    public void hasDescription() {
+        assertEquals("A Succulent Chinese Meal", repo.getDescription());
+    }
+
+    @Test
+    public void hasRepoType() {
+        assertEquals(RepoType.PUBLIC, repo.getRepoType());
+    }
+
+    @Test
+    public void commitListStartsEmpty() {
+        assertEquals(0, repo.commitCount());
+    }
+
+    @Test
+    public void canAddCommit() {
+        repo.addCommit(commit1);
+        assertEquals(1, repo.commitCount());
+    }
+
+    @Test
+    public void canGetCommitById() {
+        repo.addCommit(commit2);
+        repo.getCommitById()
+        assertEquals();
+    }
 }
